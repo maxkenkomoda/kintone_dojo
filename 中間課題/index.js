380b2aa6728ca65f4e076dca70c7c5fd1878cdd0
MAX KEN KOMODA <58941838+maxkenkomoda@users.noreply.github.com>
2022-07-01 09:42:34 +0900

    add comment and chang names of functions

--- 中間課題/index.js
+++ 中間課題/index.js
@@ -6,14 +6,13 @@
   // 変数の定義
   const dojoUrl =
     'https://54o76ppvn8.execute-api.ap-northeast-1.amazonaws.com/prod/bb-dojo';
-
   const newsTableBody = document.getElementById('news-table-body');
   const options = document.getElementsByClassName('option');
   const maxLength = 30;
 
   /**
    *
-   * @param {string} _query ex limit 3, order by day descなど
+   * @param {string} _query ex limit 3, order by day descなど。引数を省略しても可。
    * 本来なら引数のチェックも行いたいですが、組み合わせが膨大なので、割愛します。。。。。
    * @returns
    */
@@ -23,7 +22,7 @@
       const response = await axios.get(dojoUrl, {
         params: {
           id: 'dojo',
-          query: query,
+          query,
         },
       });
       return response.data;
@@ -33,26 +32,27 @@
   };
 
   /**
-   *
+   * 指定のHTMLテーブルのbodyを作成する関数
    * @param {data} data axiosで取得したデータを想定
    */
   const createTableBody = (data) => {
+    // 一度テーブルデータを削除
+    newsTableBody.innerHTML = '';
     const dataKeys = Object.keys(data);
     dataKeys.forEach((key) => {
       const newRow = newsTableBody.insertRow();
-      newRow.id = key;
 
-      // 日付セル作成
-      createTableElement(data[key].day.value, newRow, 'day');
-      // カテゴリセル作成
-      createTableElement(
+      // 日付の<td>作成
+      createTableData(data[key].day.value, newRow, 'day');
+      // カテゴリ用の<td>作成
+      createTableData(
         data[key].category.value,
         newRow,
         `${data[key].label.value}`
       );
 
-      //コンテンツセル作成
-      createTableAnchorElement(
+      //タイトル用の<td>作成
+      createAnchorInTableData(
         data[key].content.value,
         newRow,
         data[key].url.value,
@@ -63,25 +63,25 @@
   };
 
   /**
-   *
+   * 引数をもとに<td></td>を作成する関数
    * @param {string} text innerTextとして使う
-   * @param {HTML Element} targetRow 指定の列
-   * @param {string} className cssで使用するため
+   * @param {HTMLElement} targetRow 指定の列
+   * @param {string} className cssで使用するClass名を指定
    */
-  const createTableElement = (text, targetRow, className) => {
+  const createTableData = (text, targetRow, className) => {
     const newCell = targetRow.insertCell();
     newCell.innerText = text;
     newCell.classList = className;
   };
 
   /**
-   *
+   * 引数をもとに、<td><a></a></td>を作る関数
    * @param {string} text innerTextとして使う
-   * @param {HTML Element} targetRow 指定の列
+   * @param {HTMLElement} targetRow 指定の列
    * @param {string} link URL
    * @param {string} target target=_blankなどを想定
    */
-  const createTableAnchorElement = (
+  const createAnchorInTableData = (
     text,
     targetRow,
     link,
@@ -90,19 +90,27 @@
   ) => {
     const newCell = targetRow.insertCell();
     newCell.classList = className;
+
     const newAnchor = document.createElement('a');
     newAnchor.innerText = truncateSentence(text);
+    //以下2行ハードコードな気がしますが、繰り返しを使うとさらに行数を使うので、今回はハードコードで。。
     newAnchor.setAttribute('href', link);
     newAnchor.setAttribute('target', target);
+
     newCell.appendChild(newAnchor);
   };
 
+  /**
+   * 引数の文字列がmaxLength以上の場合、...をつける関数
+   * @param {string} text
+   * @returns
+   */
   const truncateSentence = (text) => {
-    return text.length > maxLength ? `${text.substr(0, maxLength)}...` : text;
+    return text.length > maxLength ? `${text.slice(0, maxLength)}...` : text;
   };
 
   /**
-   * ページが呼び込まれたら実行されるやつ
+   * ページが呼び込まれたら実行される
    */
   getNews('order by day desc').then((data) => {
     createTableBody(data);
@@ -114,10 +122,7 @@
   for (const option of options) {
     option.addEventListener('change', () => {
       const query = `${options[1].value} ${options[0].value}`;
-
       getNews(query).then((data) => {
-        //一度テーブルを削除
-        newsTableBody.innerHTML = '';
         createTableBody(data);
       });
     });
